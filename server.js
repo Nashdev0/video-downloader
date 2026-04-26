3dfa79b6cd8638d5a3c0e606470a441ecb7ada3c
NashDev0 <nashrullahzuhdilbs@gmail.com>
2026-04-26 10:31:40 +0700

    repisi

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000; // Switch back to 5000 since there is no 
 app.use(compression());
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
 /*
  * ============================================================================
